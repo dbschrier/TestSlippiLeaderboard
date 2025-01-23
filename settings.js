724237ee241ff255181a290be4d96a5927ddbf35
Samantha Jabbour <sammiejab@hotmail.com>
2025-01-23 13:05:59 -0500

    gg

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Base application title used in document.title
-  title: 'TestMTL Ranked Slippi Leaderboard',
+  title: 'Slippi Ranked Grandmaster Tracker',
 
   // use cname option to add CNAME file to webpack build
   // CNAME file allows to use custom domain names with gh-pages, example:
