a56427768717830089846aa5708023633d525b56
Samantha Jabbour <sammiejab@hotmail.com>
2025-02-14 12:24:55 -0500

    daily gm limit update1402

--- src/components/routes/home/HomePage.tsx
+++ src/components/routes/home/HomePage.tsx
@@ -54,7 +54,7 @@ export default function HomePage() {
 
   return (
     <div className="flex flex-col items-center h-screen p-8"
-      <h1 className="text-3xl m-4 text-center text-white">YOU NEED 2260.0 FOR GRANDMASTER ON 13TH FEBRUARY 2025</h1>
+      <h1 className="text-3xl m-4 text-center text-white">YOU NEED 2267.0 FOR GRANDMASTER ON 14TH FEBRUARY 2025</h1>
     
       <img className="h-48" src={ColoradoFlag} alt="colorado flag" />
       <h1 className="text-3xl m-4 text-center text-white">
