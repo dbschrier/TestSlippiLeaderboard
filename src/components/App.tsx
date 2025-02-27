af9424b6d7de091398da01262144f10929678df3
Samantha Jabbour <sammiejab@hotmail.com>
2025-02-27 02:46:50 -0500

    gm limit page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import HomePage from './routes/home/HomePage';
 import settings from '../../settings';
+import GmRank from "./routes/GmRank";
 
 export default function App() {
   return (
     <HashRouter basename={settings.repoPath}>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/GmRank" component={GmRank} />
       </Switch>
     </HashRouter>
   );
---
+++ src/components/routes/GmRank.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NewPage: React.FC = () => {
+  return (
+    <div>
+      <h1>2280 for GM | 26 Feb</h1>
+    </div>
+  );
+};
+
+export default NewPage;
