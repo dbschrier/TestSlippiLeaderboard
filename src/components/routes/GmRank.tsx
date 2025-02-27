737091e004bf9034ce7fb7ba7b17c5d65db42e0b
Samantha Jabbour <sammiejab@hotmail.com>
2025-02-27 02:48:58 -0500

    gm limit page

--- src/components/routes/GmRank.tsx
+++ src/components/routes/GmRank.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const NewPage: React.FC = () => {
   return (
     <div>
-      <h1>2280 for GM | 26 Feb</h1>
+      <h1 className='text-red'>2280 for GM | 26 Feb</h1>
     </div>
   );
 };
