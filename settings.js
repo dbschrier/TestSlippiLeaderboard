4b7b0587629e8d9a55a3974afa9284d20645efa6
dbschrier <dbschrier@gmail.com>
2025-04-06 21:41:43 -0400

    changed repo path

--- settings.js
+++ settings.js
@@ -9,7 +9,7 @@ module.exports = {
 
   // add repo path for username.github.io/repoPath for react router to recognize paths,
   // use only when deployed to github.io, leave as null for deployments with custom domains
-  repoPath: 'https://confusedsammie.github.io/TestSlippiLeaderboard/',
+  repoPath: 'https://dbschrier.github.io/TestSlippiLeaderboard/',
 
   // Google sheets ID for player connect codes.
   // https://docs.google.com/spreadsheets/d/[YOUR ID]
